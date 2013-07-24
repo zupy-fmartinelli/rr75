ad549827706e476427b4d0ca7494770537fd1982
Fabio Martinelli <androidado@gmail.com>
2013-07-23 22:53:34 -0300

    Aula 2

---
+++ AutoShop/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AutoShop::Application.config.secret_token = '9c25525d619801ea356d95226f5f39fc6f5b9486b0702788b8742c5d0726ea38fe922c90399c094592ea087d4e0d94b8ddaf1222b171636db74ef6df96001ef4'
